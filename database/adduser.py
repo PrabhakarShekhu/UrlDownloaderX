1073054341ea0fee818f14ad5a084eb27b9f1c40
Prabhakar Shekhu <65145790+PrabhakarShekhu@users.noreply.github.com>
2022-07-12 10:35:59 +0530

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -3,6 +3,6 @@ from database.access import botly
 from pyrogram.types import Message
 
 
-async def AddUser(bot: Botly, update: Message):
+async def AddUser(bot: Client, update: Message):
     if not await botly.is_user_exist(update.from_user.id):
            await botly.add_user(update.from_user.id)
